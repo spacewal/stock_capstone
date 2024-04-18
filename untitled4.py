a2463122663c0e50b1e3b624f6b7bbb3c429d754
Alberto Diaz <103618601+spacewal@users.noreply.github.com>
2024-04-18 00:49:15 -0400

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -8,7 +8,7 @@ import datetime as dt
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from keras.layers import Dense, LSTM, Dropout, AdditiveAttention, Permute, Reshape, Multiply
 
 # Streamlit page setup
 st.title('S&P 500 Stock Analysis')
@@ -62,7 +62,7 @@ y_train, y_test = y[:train_size], y[train_size:]
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-with st.spinner('Training the model...'):
+with st.spinner('Training the model 1...'):
     model = Sequential()
 
     model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
@@ -74,4 +74,33 @@ with st.spinner('Training the model...'):
     # Train the model
     history = model.fit(X_train, y_train, epochs=100, batch_size=25, validation_split=0.2)
 
+st.success('Training complete!')
+
+with st.spinner('Training the model 2...'):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(LSTM(units=50, return_sequences=True))
+    attention = AdditiveAttention(name = 'attention_weight')
+    model.add(Permute((2,1)))
+    model.add(Reshape((-1, X_train.shape[1])))
+    attention_result = attention([model.output, model.output])
+    multiply_layer = Multiply()([model.output, attention_result])
+
+    # Return to original shape
+    model.add(Permute((2, 1)))
+    model.add(Reshape((-1, 50)))
+
+    # Adding a Flatten layer before the final Dense layer
+    model.add(tf.keras.layers.Flatten())
+
+    # Final Dense layer
+    model.add(Dense(1))
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Train the model
+    history = model.fit(X_train, y_train, epochs=100, batch_size=25, validation_split=0.2)
+
+st.success('Training complete!')
 st.success('Training complete!')
